05ce947f5a7230de62dbb1f164e65abfe4618b1e
AlexisGorospe <AlexisGorospe@github.com>
2025-04-02 19:42:12 -0400

    it's not working properky

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { MissionlistComponent } from './missionlist/missionlist.component';
 import { TestlistComponent } from './test/test.component';
+import { MissiondetailsComponent } from './missiondetails/missiondetails.component';
 
 export const routes: Routes = [
-    {path: "mission", component: MissionlistComponent},
-    {path: "missiondetails", component: MissionlistComponent},
+    {path: "missions", component: MissiondetailsComponent},
+    {path: "missionlist", component: MissionlistComponent},
     {path: "test", component: TestlistComponent}
 ];
--- src/app/missiondetails/missiondetails.component.ts
+++ src/app/missiondetails/missiondetails.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-missiondetails',
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './missiondetails.component.css'
 })
 export class MissiondetailsComponent {
-  
+  private http = inject(HttpClient);
+  post: any;
+
+  constructor() {
+      this.http.get('https://api.spacexdata.com/v3/launches?launch_year=2020')
+          .subscribe(data => {
+              this.post = data;
+          });
+  }
+
+  search() {
+    let year = (<HTMLInputElement>document.getElementById("year")).value
+    
+    this.http.get('https://api.spacexdata.com/v3/launches?launch_year=' + year)
+        .subscribe(data => {
+            this.post = data;
+        });
+  }
 }
